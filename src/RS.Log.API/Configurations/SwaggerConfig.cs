58f8e7fe822d2188f61b1b992d681cc3448efb09
Rafael Secco <raf.secco@gmail.com>
2021-10-21 07:38:04 -0300

    Refatorando a classe Startup

---
+++ src/RS.Log.API/Configurations/SwaggerConfig.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RS.Log.API.Configurations
+{
+	public static class SwaggerConfig
+	{
+		public static IServiceCollection AddSwaggerConfigureServices(this IServiceCollection services)
+		{
+			services.AddSwaggerGen(c =>
+			{
+				c.SwaggerDoc("v1", new OpenApiInfo { Title = "RS.Log.API", Version = "v1" });
+			});
+
+			return services;
+		}
+
+		public static IApplicationBuilder UseSwaggerConfigure(this IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseSwagger();
+				app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RS.Log.API v1"));
+			}
+
+			return app;
+		}
+	}
+}
