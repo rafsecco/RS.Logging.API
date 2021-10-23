0dff045c5668415f5c6da4ab6abc5fd7f989dc9e
Rafael Secco <raf.secco@gmail.com>
2021-10-23 13:16:16 -0300

    Adicionado metodo Find para aplicar filtro na consulta

---
+++ src/RS.Log.API/ViewModels/LogViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RS.Log.API.ViewModels
+{
+	public class LogViewModel
+	{
+		[Required]
+		public string TenantId { get; set; }
+		public DateTime DateTimeIni { get; set; }
+		public DateTime DateTimeEnd { get; set; }
+		public string Message { get; set; }
+	}
+}
