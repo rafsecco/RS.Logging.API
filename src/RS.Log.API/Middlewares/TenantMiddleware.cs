24bf5a84a6c441eae5296e8fe9e4ab74f0c1b431
Rafael Secco <raf.secco@gmail.com>
2021-10-23 08:03:37 -0300

    Aplicando a estratégia de MultiTenant na aplicação para separar os logs por cliente/projeto

---
+++ src/RS.Log.API/Middlewares/TenantMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using RS.Log.API.Extensions;
+using RS.Log.API.Provider;
+using System.Threading.Tasks;
+
+namespace RS.Log.API.Middlewares
+{
+	public class TenantMiddleware
+	{
+		private readonly RequestDelegate _next;
+
+		public TenantMiddleware(RequestDelegate next)
+		{
+			_next = next;
+		}
+		
+		public async Task InvokeAsync(HttpContext httpContext)
+		{
+			var tenant = httpContext.RequestServices.GetRequiredService<TenantData>();
+			tenant.TenantId = httpContext.GetTenantId();
+			await _next(httpContext);
+		}
+	}
+}
