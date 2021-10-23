4fd2434bb86a8f99b53b720258dbe3e59c810896
Rafael Secco <raf.secco@gmail.com>
2021-10-23 11:48:06 -0300

    Adicionado metodo para inserir logs

--- src/RS.Log.API/Data/ApplicationContext.cs
+++ src/RS.Log.API/Data/ApplicationContext.cs
@@ -21,14 +21,6 @@ namespace RS.Log.API.Data
 				.HasQueryFilter(p => p.TenantId == _tenant.TenantId)
 				.HasIndex(idx => new { idx.DateCreated, idx.TenantId })
 				.HasDatabaseName("idx_tenantid");
-
-			modelBuilder.Entity<Domain.Log>().HasData(
-				new Domain.Log { Id = 1, Message = "teste 1", TenantId = "tenant-1" },
-				new Domain.Log { Id = 2, Message = "teste 2", TenantId = "tenant-2" },
-				new Domain.Log { Id = 3, Message = "teste 3", TenantId = "tenant-2" }
-			);
-
-
 		}
 	}
 }
