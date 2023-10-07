9ebc7da67ddd46f8cd4955db85859839060d387c
Rafael Secco <raf.secco@gmail.com>
2023-10-07 13:21:25 -0300

    Criando o frontend criando a pagina para criar um log

---
+++ src/RS.Logging.FrontEnd.Angular/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { NewComponent } from './components/new/new.component';
+
+@NgModule({
+	declarations: [
+		AppComponent,
+		NewComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		ReactiveFormsModule,
+		HttpClientModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
+})
+export class AppModule { }
